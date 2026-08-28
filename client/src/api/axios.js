47b5f3e060baf55306db284c17499a9552aa1165
omar <benkacemomar6@gmail.com>
2026-08-28 14:09:52 +0100

    Add fronten enc varibale for url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = 'http://localhost:3000/api'
+const baseURL = import.meta.env.VITE_API_URL
 const api=axios.create({baseURL});
 api.interceptors.request.use((config)=>{
     const token=localStorage.getItem('token')
